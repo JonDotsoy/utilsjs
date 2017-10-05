8c894b43dad87a82f355c0a75ef6769cbf5a0b4b
Jonathan Delgado Zamorano <hi@jon.soy>
2017-10-05 19:04:16 -0300

    reducer runtime filterAsync

--- Array/filterAsync/index.js
+++ Array/filterAsync/index.js
@@ -7,9 +7,7 @@ Object.defineProperty(exports, '__esModule', {
 exports.filterAsync = function filterAsync(arr, fn) {
   return new Promise(function (resolve, reject) {
     Promise.all(arr.map(fn)).then(function (results) {
-      return results.map(function (e, i, c) {
-        return arr[i];
-      }).filter(function (e, i, c) {
+      return arr.filter(function (e, i, c) {
         return results[i];
       });
     }).then(resolve).catch(reject);
